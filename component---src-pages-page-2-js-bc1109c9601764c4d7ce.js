c6cb2224a475f0e76243d4bc9dbf297da6a67cd7
saniok017 <saniok017@gmail.com>
2019-01-26 02:15:35 +0300

    Updates

--- component---src-pages-page-2-js-bc1109c9601764c4d7ce.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{139:function(e,t,a){"use strict";a.r(t);var n=a(0),o=a.n(n),l=a(142),c=a(150),r=a(149);t.default=function(){return o.a.createElement(c.a,null,o.a.createElement(r.a,{title:"Page two"}),o.a.createElement("h1",null,"Hi from the second page"),o.a.createElement("p",null,"Welcome to page 2"),o.a.createElement(l.Link,{to:"/"},"Go back to the homepage"))}}}]);
-//# sourceMappingURL=component---src-pages-page-2-js-bc1109c9601764c4d7ce.js.map
